530d49cb6cfed2c8254d71b866d8b94faf1e5178
akarsh seggemu <sakarshkumar@gmail.com>
2020-04-17 22:37:05 +0200

    Create AwardsLayout.dart

---
+++ lib/AwardsLayout.dart
@@ -0,0 +1,25 @@
+/*
+ * Copyright © 2020 Akarsh Seggemu. All rights reserved.
+ */
+
+import 'package:flutter/material.dart';
+import 'dart:io';
+
+import 'package:akarsh_seggemu_resume/ResumeJSONSchema.dart';
+
+class AwardsLayout extends StatefulWidget {
+  final Basics basics;
+  AwardsLayout({Key key, @required this.basics}) : super(key: key);
+
+  @override
+  _AwardsLayoutScreenState createState() => _AwardsLayoutScreenState();
+}
+
+class _AwardsLayoutScreenState extends State<AwardsLayout> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+
+}
